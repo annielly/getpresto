1c6dfc63b5951ccd48f542197fdfcdca5db1e678
annielly <love.annie.ly@gmail.com>
2023-06-04 00:50:12 -0700

    Mostly calendar component
    -added br to bottom of table section on styling page
    -deleted 360 media query
    -added calendar to component page, reworked code to respond to presto website

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,81 +1,157 @@
-let today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
-
-let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
+const holidays = [
+  {
+    hdate: "01-01-2023",
+    holiday: "New Year's Day",
+  },
+  {
+    hdate: "06-17-2023",
+    holiday: "JAM SESH",
+  },
+  {
+    // To add desired holidays or special events, input like the format below to this array
+    // hdate: "25-12-2023",
+    // holiday: "Christmas",
+  },
 ];
 
-let monthYearHead = document.getElementById("monthYearHead");
+const calendar = document.querySelector("#calendar");
+const monthBanner = document.querySelector("#month");
+let navigation = 0;
+let clicked = null;
+let events = localStorage.getItem("events") ? JSON.parse(localStorage.getItem("events")) : [];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-showCalendar(currentMonth, currentYear);
+function loadCalendar() {
+  const dt = new Date();
 
-function showCalendar(month, year){
-    let firstDay = new Date(year, month).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+  if (navigation != 0) {
+    dt.setMonth(new Date().getMonth() + navigation);
+  }
+  const day = dt.getDate();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
+  monthBanner.innerText = `${dt.toLocaleDateString("en-us", {
+    month: "long",
+  })} ${year}`;
+  calendar.innerHTML = "";
+  const dayInMonth = new Date(year, month + 1, 0).getDate();
+  const firstDayofMonth = new Date(year, month, 1);
+  const dateText = firstDayofMonth.toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
 
-    let tbl = document.getElementById("calBody");
+  const dayString = dateText.split(", ")[0];
+  const emptyDays = weekdays.indexOf(dayString);
 
-    tbl.innerHTML = "";
+  for (let i = 1; i <= dayInMonth + emptyDays; i++) {
+    const dayBox = document.createElement("div");
+    dayBox.classList.add("day");
+    const monthVal = month + 1 < 10 ? "0" + (month + 1) : month + 1;
+    const dateVal = i - emptyDays < 10 ? "0" + (i - emptyDays) : i - emptyDays;
+    const dateText = `${dateVal}-${monthVal}-${year}`;
+    if (i > emptyDays) {
+      dayBox.innerText = i - emptyDays;
+      const eventOfTheDay = events.find((e) => e.date == dateText);
+      const holidayOfTheDay = holidays.find((e) => e.hdate == dateText);
 
-    monthYearHead.innerHTML = months[month] + " " + year;
-    
-    let date = 1;
+      if (i - emptyDays === day && navigation == 0) {
+        dayBox.id = "currentDay";
+      }
 
-    for(let i = 0; i < 6; i++){
-        let row = document.createElement("tr");
-        
-        for(let j = 0; j < 7; j++){
-            if(i === 0 && j < firstDay){
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            } else if(date > daysInMonth){
-                break;
-            } else {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                date++;
-            }
+      if (eventOfTheDay) {
+        const eventDiv = document.createElement("div");
+        eventDiv.classList.add("event");
+        eventDiv.innerText = eventOfTheDay.title;
+        dayBox.appendChild(eventDiv);
+      }
+      if (holidayOfTheDay) {
+        const eventDiv = document.createElement("div");
+        eventDiv.classList.add("event");
+        eventDiv.classList.add("holiday");
+        eventDiv.innerText = holidayOfTheDay.holiday;
+        dayBox.appendChild(eventDiv);
+      }
 
-            // date++;
-        }
-
-        tbl.appendChild(row);
+      dayBox.addEventListener("click", () => {
+        showModal(dateText);
+      });
+    } else {
+      dayBox.classList.add("plain");
     }
+    calendar.append(dayBox);
+  }
+}
+function buttons() {
+  const btnBack = document.querySelector("#btnBack");
+  const btnNext = document.querySelector("#btnNext");
+  const btnDelete = document.querySelector("#btnDelete");
+  const btnSave = document.querySelector("#btnSave");
+  const closeButtons = document.querySelectorAll(".btnClose");
+  const txtTitle = document.querySelector("#txtTitle");
+
+  btnBack.addEventListener("click", () => {
+    navigation--;
+    loadCalendar();
+  });
+  btnNext.addEventListener("click", () => {
+    navigation++;
+    loadCalendar();
+  });
+  modal.addEventListener("click", closeModal);
+  closeButtons.forEach((btn) => {
+    btn.addEventListener("click", closeModal);
+  });
+  btnDelete.addEventListener("click", function () {
+    events = events.filter((e) => e.date !== clicked);
+    localStorage.setItem("events", JSON.stringify(events));
+    closeModal();
+  });
+
+  btnSave.addEventListener("click", function () {
+    if (txtTitle.value) {
+      txtTitle.classList.remove("error");
+      events.push({
+        date: clicked,
+        title: txtTitle.value.trim(),
+      });
+      txtTitle.value = "";
+      localStorage.setItem("events", JSON.stringify(events));
+      closeModal();
+    } else {
+      txtTitle.classList.add("error");
+    }
+  });
 }
 
-function previous(){
-    currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
-    currentMonth = currentMonth === 0 ? 11: currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
+const modal = document.querySelector("#calModal"); 
+const viewEventForm = document.querySelector("#viewEvent");
+const addEventForm = document.querySelector("#addEvent");
+
+function showModal(dateText) {
+  clicked = dateText;
+  const eventOfTheDay = events.find((e) => e.date == dateText);
+  if (eventOfTheDay) {
+    //Event already Preset
+    document.querySelector("#eventText").innerText = eventOfTheDay.title;
+    viewEventForm.style.display = "block";
+  } else {
+    //Add new Event
+    addEventForm.style.display = "block";
+  }
+  modal.style.display = "block";
 }
 
-function next(){
-    currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
+//Close Modal
+function closeModal() {
+  viewEventForm.style.display = "none";
+  addEventForm.style.display = "none";
+  modal.style.display = "none";
+  clicked = null;
+  loadCalendar();
 }
 
-// drop down date jump
-let selectYear = document.getElementById("year");
-let selectMonth = document.getElementById("month");
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
+buttons();
+loadCalendar();
